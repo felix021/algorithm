00c80f8b9b95f545d58cc775ac9db1566fa49c96
FengMin.Felix <fengmin.felix@bytedance.com>
2020-10-15 23:27:02 +0800

    level 2 of foo bar challenge
    Change-Id: I5fbc7cb702e3023d825bc2fcf0ce4836991476e6

---
+++ foobar/2-bunny-prisoner-locating/solution.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+def solution(x, y):
+    z = x + y - 1
+
+    p = 1
+    for i in range(2, z + 1):
+        p += i - 1
+
+    return str(p + x - 1)
+
+if __name__ == "__main__":
+    print solution(3, 2) == "9"
+    print solution(5, 10) == "96"
